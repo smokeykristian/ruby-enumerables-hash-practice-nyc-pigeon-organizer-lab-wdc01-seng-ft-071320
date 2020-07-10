3d84bed15110ebea842fa2574d11ff88e58d9f19
Kristian Portillo <portillk@gmail.com>
2020-07-10 09:42:02 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,6 +1,7 @@
 def nyc_pigeon_organizer(data)
  final_results = data.each_with_object({}) do |(key, value), final_array|
    value.each do |name| 
+     name.each do |name|
      if !final_array[name]
         final_array[name] = {}
     end
